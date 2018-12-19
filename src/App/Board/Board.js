a923d9244568147fecadd4e2ee5640963d2e2fba
Bartłomiej Chmielarz <bartlomiej.chmielarz@gmail.com>
2018-12-19 13:28:29 +0100

    Card creation

--- src/App/Board/Board.js
+++ src/App/Board/Board.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Board.css';
-import {Button, UncontrolledButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import { UncontrolledButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import CreateList from "./CreateList";
 import CardModal from "./Card/CardModal";
 
@@ -10,11 +10,12 @@ export default class Board extends React.Component {
         super(props);
 
         this.state = {
-            board: [],
+            board: [null],
             isCreateListOpen: false,
             isCardOpen: false,
             dragCardId: "",
             dragListId: "",
+            isAddCardEditable: [],
         };
     }
     componentDidMount() {
@@ -26,14 +27,15 @@ export default class Board extends React.Component {
         }).then(data => this.setState({board: data}))
 
     }
+
     createRow() {
         let row = [];
         let list = this.state.board.lists;
         let boardID = this.state.board.id;
         console.log(boardID);
-
         if(list) {
             for (let i = 0; i < list.length; i++) {
+
                 let cards = list[i].cards;
                 row.push(
                     <div className="col-md-3" key={list[i].id} onDragOver={(e) => e.preventDefault()}
@@ -53,7 +55,9 @@ export default class Board extends React.Component {
                                     </div>
                                 </div>
                                 {this.fillList(cards,list[i].id)}
-                                <Button className="addCard" color="link">Dodaj nową kartę</Button>
+                                <div className="addCard">
+                                    <input type="text" name={list[i].id} placeholder="Dodaj Kartę" onKeyPress={this.handleCardCreationField} />
+                                </div>
                             </div>
 
                         </div>
@@ -67,8 +71,38 @@ export default class Board extends React.Component {
                         <CreateList id={boardID}/>
                     </div>
                 </div>
-            </div>)
+            </div>);
         return row;
+    }
+
+    handleCardCreationField = (e) => {
+        if (e.key === 'Enter') {
+            if(e.target.value !== "") {
+                console.log(e.target.value + ' id: ' + e.target.name);
+                this.requestCardCreation(e);
+                e.target.value = "";
+
+            }
+        }
+    };
+
+    requestCardCreation(event){
+        event.preventDefault();
+        let boardID= document.location.href.split('/')[4];
+        let listID= event.target.name;
+        let data = event.target.value;
+        fetch('http://localhost:7000/boards/'+boardID+'/lists/'+listID+'/cards', {
+            method: 'POST',
+            body: JSON.stringify({cardName: data}),
+            credentials: 'include'
+        }).then(data => {
+            if (data.status === 200) {
+                window.location.reload();
+            }
+            else{
+                console.log("CREATION ERROR");
+            }
+        });
     }
 
     fillList(cards, listId){
@@ -102,7 +136,7 @@ export default class Board extends React.Component {
         var selectedArray = array.lists.find(data => data.id === this.state.dragListId);
         var modifiedIndex = array.lists.indexOf(selectedArray);
 
-        var selectedCard = selectedArray.cards.find(data => data.id === this.state.dragCardId)
+        var selectedCard = selectedArray.cards.find(data => data.id === this.state.dragCardId);
         var modifiedCards = selectedArray.cards.filter(data => data.id !== this.state.dragCardId);
         selectedArray.cards = modifiedCards;
         array.lists[modifiedIndex] = selectedArray;
